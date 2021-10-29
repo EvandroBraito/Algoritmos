110bd9235484cb1045d279f5634506fde549537f
EvandroBraito <evandro.simoes01@fatec.sp.gov.br>
2021-10-29 18:38:41 -0300

    Adicionando jogo da velha

---
+++ c_cpp/jogo_da_velha.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define iguais(a, b, c) (a != ' ' && a == b && b == c)
+
+void exibe(char m[3][3])
+{
+    system("clear");
+    for (int i = 0; i < 3; i++)
+    {
+        printf(" %c | %c | %c \n", m[i][0], m[i][1], m[i][2]);
+        if (i < 2)
+            puts("---+---+---");
+    }
+}
+
+
+int vencedor(char m[3][3])
+{
+    for (int i = 0; i < 3; i++)
+    {
+        if (iguais(m[i][0], m[i][1], m[i][2]))
+        {
+            return m[i][0];
+        }
+
+        if (iguais(m[0][i], m[1][i], m[2][i]))
+        {
+            return m[0][i];
+        }
+    }
+
+    if (iguais(m[0][0], m[1][1], m[2][2]))
+    {
+        return m[0][0];
+    }
+
+    if (iguais(m[0][2], m[1][1], m[2][0]))
+    {
+        return m[0][2];
+    }
+
+    return ' ';
+}
+
+
+int main(void)
+{
+    char m[3][3] = {{' ', ' ', ' '},
+                    {' ', ' ', ' '},
+                    {' ', ' ', ' '}};
+
+    exibe(m);
+
+    printf("\nVencedor: %c\n\n", vencedor(m));
+
+    return 0;
+}
